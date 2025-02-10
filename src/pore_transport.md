015a69f1deaa676476f15ec79010dad9077c1ce4
Katharina Paul <katharina.paul@rwth-aachen.de>
2025-02-10 16:34:38 +0100

    add film_diffusion for optimization

--- src/pore_transport.md
+++ src/pore_transport.md
@@ -1,30 +1,29 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: md:myst,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.16.6
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
+---
+jupytext:
+  formats: md:myst,py:percent
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.16.6
+kernelspec:
+  display_name: Python 3 (ipykernel)
+  language: python
+  name: python3
+---
 
-# %% [markdown]
-# # Fit Particle Porosity
-#
-# Particle porosity is an important consideration in the modeling of column transport. The speed in which a tracer is able to pass through the column is dependent on its interaction with the stationary phase. The porosity is a chemical property of the particles in the stationary phase. The tracer in the mobile phase must be able to penetrate the pores to interact with the stationary phase.
-#
-# ## Experiment
-#
-# To fit the particle porosity an experiment is conducted with acetone as a pore penetrating tracer. The tracer is injected into the column and its concentration at the column outlet is measured and compared to the concentration predicted by simulation results.
-# - Acetone (pore penetrating tracer './experimental_data/pore_penetrating_tracer.csv')
-# - data: time / s and c / mM
+# Fit Pore Transport
 
-# %%
+Particle porosity is an important consideration in the modeling of column transport. The speed in which a tracer is able to pass through the column is dependent on its interaction with the stationary phase. The porosity is a chemical property of the particles in the stationary phase. The tracer in the mobile phase must be able to penetrate the pores to interact with the stationary phase.
+
+
+## Experiment
+
+To fit the particle porosity an experiment is conducted with acetone as a pore penetrating tracer. The tracer is injected into the column and its concentration at the column outlet is measured and compared to the concentration predicted by simulation results.
+- Acetone (pore penetrating tracer './experimental_data/pore_penetrating_tracer.csv')
+- data: time / s and c / mM
+
+```{code-cell} ipython3
 import numpy as np
 data = np.loadtxt('experimental_data/pore_penetrating_tracer.csv', delimiter=',')
 
@@ -38,17 +37,18 @@ tracer_peak = ReferenceIO(
 
 if __name__ == '__main__':
     _ = tracer_peak.plot(x_axis_in_minutes = False)
+```
 
-# %% [markdown]
-# ## Reference Model
-#
-# Here, initial values for `axial_dispersion` and `bed_porosity` are assumed. The `particle_porosity` will later be optimized, thus an arbitrary value can be set for now. `film_diffusion` is set to a value higher than 0 to allow for the pore penetrating tracer to enter the pores.
+## Reference Model
 
-# %%
+Here, initial values for `axial_dispersion` and `bed_porosity` are assumed. The `particle_porosity` and  `film_diffusion`  will later be optimized, thus arbitrary values can be set for now. `film_diffusion` is set to a value > 0 m/s to allow for the tracer to enter the pores.
+
+```{code-cell} ipython3
 from CADETProcess.processModel import ComponentSystem
 component_system = ComponentSystem(['Penetrating Tracer'])
+```
 
-# %%
+```{code-cell} ipython3
 from CADETProcess.processModel import Inlet, Outlet, LumpedRateModelWithPores
 
 feed = Inlet(component_system, name='feed')
@@ -65,13 +65,14 @@ column.particle_radius = 34e-6
 
 column.axial_dispersion = 1e-8
 column.bed_porosity = 0.3
-column.particle_porosity = 0.8
 
+column.particle_porosity = 0.8
 column.film_diffusion = [1]
 
 outlet = Outlet(component_system, name='outlet')
+```
 
-# %%
+```{code-cell} ipython3
 from CADETProcess.processModel import FlowSheet
 
 flow_sheet = FlowSheet(component_system)
@@ -84,8 +85,9 @@ flow_sheet.add_unit(outlet)
 flow_sheet.add_connection(feed, column)
 flow_sheet.add_connection(eluent, column)
 flow_sheet.add_connection(column, outlet)
+```
 
-# %%
+```{code-cell} ipython3
 from CADETProcess.processModel import Process
 
 Q_ml_min = 0.5  # ml/min
@@ -120,22 +122,22 @@ process.add_event(
     0,
     process.cycle_time
 )
+```
 
-# %% [markdown]
-# ## Simulator
+## Simulator
 
-# %%
+```{code-cell} ipython3
 from CADETProcess.simulator import Cadet
 simulator = Cadet()
 
 if __name__ == '__main__':
     simulation_results = simulator.simulate(process)
     _ = simulation_results.solution.outlet.inlet.plot()
+```
 
-# %% [markdown]
-# ## Comparator
+## Comparator
 
-# %%
+```{code-cell} ipython3
 from CADETProcess.comparison import Comparator
 
 comparator = Comparator()
@@ -146,11 +148,11 @@ comparator.add_difference_metric(
 
 if __name__ == '__main__':
     comparator.plot_comparison(simulation_results)
+```
 
-# %% [markdown]
-# ## Optimization Problem
+## Optimization Problem
 
-# %%
+```{code-cell} ipython3
 from CADETProcess.optimization import OptimizationProblem
 
 optimization_problem = OptimizationProblem('particle_porosity')
@@ -161,6 +163,12 @@ optimization_problem.add_variable(
     name='particle_porosity',
     parameter_path='flow_sheet.column.particle_porosity',
     lb=0.5, ub=0.99,
+    transform='auto'
+)
+optimization_problem.add_variable(
+    name='film_diffusion',
+    parameter_path='flow_sheet.column.film_diffusion',
+    lb=0.001, ub=0.99,
     transform='auto'
 )
 
@@ -181,50 +189,50 @@ def callback(simulation_results, individual, evaluation_object, callbacks_dir='.
 
 
 optimization_problem.add_callback(callback, requires=[simulator])
+```
 
-# %% [markdown]
-# ## Optimizer
+## Optimizer
 
-# %%
+```{code-cell} ipython3
 from CADETProcess.optimization import U_NSGA3
 optimizer = U_NSGA3()
 optimizer.n_max_gen = 3
 optimizer.pop_size = 3
 optimizer.n_cores = 3
+```
 
-# %% [markdown]
-# ## Run Optimization
+## Run Optimization
 
-# %%
+```{code-cell} ipython3
 optimization_results = optimizer.optimize(
     optimization_problem,
     use_checkpoint=False )
+```
 
-# %% [markdown]
-# ### Optimization Progress and Results
-#
-# The `OptimizationResults` which are returned contain information about the progress of the optimization.
-# For example, the attributes `x` and `f` contain the final value(s) of parameters and the objective function.
+### Optimization Progress and Results
 
-# %%
+The `OptimizationResults` which are returned contain information about the progress of the optimization.
+For example, the attributes `x` and `f` contain the final value(s) of parameters and the objective function.
+
+```{code-cell} ipython3
 print(optimization_results.x)
 print(optimization_results.f)
+```
 
-# %% [markdown]
-# After optimization, several figures can be plotted to vizualize the results. For example, the convergence plot shows how the function value changes with the number of evaluations.
+After optimization, several figures can be plotted to vizualize the results. For example, the convergence plot shows how the function value changes with the number of evaluations.
 
-# %%
+```{code-cell} ipython3
 optimization_results.plot_convergence()
+```
 
-# %% [markdown]
-# The plot_objectives method shows the objective function values of all evaluated individuals. Here, lighter color represent later evaluations. Note that by default the values are plotted on a log scale if they span many orders of magnitude. To disable this, set autoscale=False.
+The plot_objectives method shows the objective function values of all evaluated individuals. Here, lighter color represent later evaluations. Note that by default the values are plotted on a log scale if they span many orders of magnitude. To disable this, set autoscale=False.
 
-# %%
+```{code-cell} ipython3
 optimization_results.plot_objectives()
+```
 
-# %% [markdown]
-# All figures are saved automatically in the `working_directory`.
-# Moreover, results are stored in a `.csv` file.
-# - The `results_all.csv` file contains information about all evaluated individuals.
-# - The `results_last.csv` file contains information about the last generation of evaluated individuals.
-# - The `results_pareto.csv` file contains only the best individual(s).
+All figures are saved automatically in the `working_directory`.
+Moreover, results are stored in a `.csv` file.
+- The `results_all.csv` file contains information about all evaluated individuals.
+- The `results_last.csv` file contains information about the last generation of evaluated individuals.
+- The `results_pareto.csv` file contains only the best individual(s).
--- src/pore_transport.py
+++ src/pore_transport.py
@@ -1,28 +1,31 @@
----
-jupytext:
-  formats: md:myst,py:percent
-  text_representation:
-    extension: .md
-    format_name: myst
-    format_version: 0.13
-    jupytext_version: 1.16.6
-kernelspec:
-  display_name: Python 3 (ipykernel)
-  language: python
-  name: python3
----
+# ---
+# jupyter:
+#   jupytext:
+#     formats: md:myst,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.6
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
 
-# Fit Particle Porosity
+# %% [markdown]
+# # Fit Pore Transport
+#
+# Particle porosity is an important consideration in the modeling of column transport. The speed in which a tracer is able to pass through the column is dependent on its interaction with the stationary phase. The porosity is a chemical property of the particles in the stationary phase. The tracer in the mobile phase must be able to penetrate the pores to interact with the stationary phase.
+#
+#
+# ## Experiment
+#
+# To fit the particle porosity an experiment is conducted with acetone as a pore penetrating tracer. The tracer is injected into the column and its concentration at the column outlet is measured and compared to the concentration predicted by simulation results.
+# - Acetone (pore penetrating tracer './experimental_data/pore_penetrating_tracer.csv')
+# - data: time / s and c / mM
 
-Particle porosity is an important consideration in the modeling of column transport. The speed in which a tracer is able to pass through the column is dependent on its interaction with the stationary phase. The porosity is a chemical property of the particles in the stationary phase. The tracer in the mobile phase must be able to penetrate the pores to interact with the stationary phase.
-
-## Experiment
-
-To fit the particle porosity an experiment is conducted with acetone as a pore penetrating tracer. The tracer is injected into the column and its concentration at the column outlet is measured and compared to the concentration predicted by simulation results.
-- Acetone (pore penetrating tracer './experimental_data/pore_penetrating_tracer.csv')
-- data: time / s and c / mM
-
-```{code-cell} ipython3
+# %%
 import numpy as np
 data = np.loadtxt('experimental_data/pore_penetrating_tracer.csv', delimiter=',')
 
@@ -36,18 +39,17 @@ tracer_peak = ReferenceIO(
 
 if __name__ == '__main__':
     _ = tracer_peak.plot(x_axis_in_minutes = False)
-```
 
-## Reference Model
+# %% [markdown]
+# ## Reference Model
+#
+# Here, initial values for `axial_dispersion` and `bed_porosity` are assumed. The `particle_porosity` and  `film_diffusion`  will later be optimized, thus arbitrary values can be set for now. `film_diffusion` is set to a value > 0 m/s to allow for the tracer to enter the pores.
 
-Here, initial values for `axial_dispersion` and `bed_porosity` are assumed. The `particle_porosity` will later be optimized, thus an arbitrary value can be set for now. `film_diffusion` is set to a value higher than 0 to allow for the pore penetrating tracer to enter the pores.
-
-```{code-cell} ipython3
+# %%
 from CADETProcess.processModel import ComponentSystem
 component_system = ComponentSystem(['Penetrating Tracer'])
-```
 
-```{code-cell} ipython3
+# %%
 from CADETProcess.processModel import Inlet, Outlet, LumpedRateModelWithPores
 
 feed = Inlet(component_system, name='feed')
@@ -64,14 +66,13 @@ column.particle_radius = 34e-6
 
 column.axial_dispersion = 1e-8
 column.bed_porosity = 0.3
-column.particle_porosity = 0.8
 
+column.particle_porosity = 0.8
 column.film_diffusion = [1]
 
 outlet = Outlet(component_system, name='outlet')
-```
 
-```{code-cell} ipython3
+# %%
 from CADETProcess.processModel import FlowSheet
 
 flow_sheet = FlowSheet(component_system)
@@ -84,9 +85,8 @@ flow_sheet.add_unit(outlet)
 flow_sheet.add_connection(feed, column)
 flow_sheet.add_connection(eluent, column)
 flow_sheet.add_connection(column, outlet)
-```
 
-```{code-cell} ipython3
+# %%
 from CADETProcess.processModel import Process
 
 Q_ml_min = 0.5  # ml/min
@@ -121,22 +121,22 @@ process.add_event(
     0,
     process.cycle_time
 )
-```
 
-## Simulator
+# %% [markdown]
+# ## Simulator
 
-```{code-cell} ipython3
+# %%
 from CADETProcess.simulator import Cadet
 simulator = Cadet()
 
 if __name__ == '__main__':
     simulation_results = simulator.simulate(process)
     _ = simulation_results.solution.outlet.inlet.plot()
-```
 
-## Comparator
+# %% [markdown]
+# ## Comparator
 
-```{code-cell} ipython3
+# %%
 from CADETProcess.comparison import Comparator
 
 comparator = Comparator()
@@ -147,11 +147,11 @@ comparator.add_difference_metric(
 
 if __name__ == '__main__':
     comparator.plot_comparison(simulation_results)
-```
 
-## Optimization Problem
+# %% [markdown]
+# ## Optimization Problem
 
-```{code-cell} ipython3
+# %%
 from CADETProcess.optimization import OptimizationProblem
 
 optimization_problem = OptimizationProblem('particle_porosity')
@@ -162,6 +162,12 @@ optimization_problem.add_variable(
     name='particle_porosity',
     parameter_path='flow_sheet.column.particle_porosity',
     lb=0.5, ub=0.99,
+    transform='auto'
+)
+optimization_problem.add_variable(
+    name='film_diffusion',
+    parameter_path='flow_sheet.column.film_diffusion',
+    lb=0.001, ub=0.99,
     transform='auto'
 )
 
@@ -182,50 +188,50 @@ def callback(simulation_results, individual, evaluation_object, callbacks_dir='.
 
 
 optimization_problem.add_callback(callback, requires=[simulator])
-```
 
-## Optimizer
+# %% [markdown]
+# ## Optimizer
 
-```{code-cell} ipython3
+# %%
 from CADETProcess.optimization import U_NSGA3
 optimizer = U_NSGA3()
 optimizer.n_max_gen = 3
 optimizer.pop_size = 3
 optimizer.n_cores = 3
-```
 
-## Run Optimization
+# %% [markdown]
+# ## Run Optimization
 
-```{code-cell} ipython3
+# %%
 optimization_results = optimizer.optimize(
     optimization_problem,
     use_checkpoint=False )
-```
 
-### Optimization Progress and Results
+# %% [markdown]
+# ### Optimization Progress and Results
+#
+# The `OptimizationResults` which are returned contain information about the progress of the optimization.
+# For example, the attributes `x` and `f` contain the final value(s) of parameters and the objective function.
 
-The `OptimizationResults` which are returned contain information about the progress of the optimization.
-For example, the attributes `x` and `f` contain the final value(s) of parameters and the objective function.
-
-```{code-cell} ipython3
+# %%
 print(optimization_results.x)
 print(optimization_results.f)
-```
 
-After optimization, several figures can be plotted to vizualize the results. For example, the convergence plot shows how the function value changes with the number of evaluations.
+# %% [markdown]
+# After optimization, several figures can be plotted to vizualize the results. For example, the convergence plot shows how the function value changes with the number of evaluations.
 
-```{code-cell} ipython3
+# %%
 optimization_results.plot_convergence()
-```
 
-The plot_objectives method shows the objective function values of all evaluated individuals. Here, lighter color represent later evaluations. Note that by default the values are plotted on a log scale if they span many orders of magnitude. To disable this, set autoscale=False.
+# %% [markdown]
+# The plot_objectives method shows the objective function values of all evaluated individuals. Here, lighter color represent later evaluations. Note that by default the values are plotted on a log scale if they span many orders of magnitude. To disable this, set autoscale=False.
 
-```{code-cell} ipython3
+# %%
 optimization_results.plot_objectives()
-```
 
-All figures are saved automatically in the `working_directory`.
-Moreover, results are stored in a `.csv` file.
-- The `results_all.csv` file contains information about all evaluated individuals.
-- The `results_last.csv` file contains information about the last generation of evaluated individuals.
-- The `results_pareto.csv` file contains only the best individual(s).
+# %% [markdown]
+# All figures are saved automatically in the `working_directory`.
+# Moreover, results are stored in a `.csv` file.
+# - The `results_all.csv` file contains information about all evaluated individuals.
+# - The `results_last.csv` file contains information about the last generation of evaluated individuals.
+# - The `results_pareto.csv` file contains only the best individual(s).
